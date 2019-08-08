dc8d13b0505f471f0b9678047aed294c9e487954
cgnicolas <cgnicolas1@gmail.com>
2019-08-08 02:12:05 -0400

    Haniel is negative

--- react-native-starter/src/modules/selector.js
+++
@@ -1,3 +0,0 @@
-export const getItems = state => state.foodItems.items
-export const getCart = state => state.foodItems.cart
-export const getTotal = state => state.foodItems.total
--- react-native-starter/src/utils/actions.js
+++
@@ -1,4 +0,0 @@
-export const createActiontype = type => ({ [type]: `module/${type}` })
-export const createActiontypes = actionTypes => actionTypes
-    .map(createActionType)
-    .reduce((types, type) => ({ ...types, ...type }), {});
